c9909bebd8b9657834d2f1b41083f0d6b6e6a52e
Mark Gray <mark@mark-UL30A.(none)>
2013-03-10 16:52:34 +0100

    Mark

--- iaip_c4/GameLogic.java
+++ iaip_c4/GameLogic.java
@@ -7,28 +7,32 @@ public class GameLogic implements IGameLogic {
     private int max = 2;
     private int min = 1;
     private int[][] board;
+    private int counter;
     
 
     public GameLogic() {
         //TODO Write your implementation for this method
     }
 
-    public GameLogic(int x, int y, int playerID, int[][] board) {
+    public GameLogic(int x, int y, int playerID, int[][] board,int counter) {
         //TODO Write your implementation for this method
         this.x = x;
         this.y = y;
         this.playerID = playerID;
+        this.board = new int[x][y];
+        this.counter = counter;
         //TODO Write your implementation for this method
         for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j<board[i].length; i++) {
+            for (int j = 0; j<board[i].length; j++) {
                 this.board[i][j] = board[i][j];
             }
         }
+       // System.out.println("New object " + counter);
     }
     
 
     public GameLogic createOther(int x, int y, int playerID) {
-        return new GameLogic(x,  y,  playerID, this.board);
+        return new GameLogic(x,  y,  playerID, this.board,this.counter);
     }
 	
     public void initializeGame(int x, int y, int playerID) {
@@ -61,10 +65,31 @@ public class GameLogic implements IGameLogic {
 
     public int decideNextMove() {
         //TODO Write your implementation for this method
+        counter = 10;
 
-        if (playerID == max)      
-            return minValue(); 
-        else return maxValue();
+        if (playerID == max) {
+             if (terminalTest()) return utility();
+            
+            Integer[] columns = getFreeColumns();
+            int v = Integer.MIN_VALUE;
+            int bestAction = -1;
+
+            for(int i = 0; i < columns.length; i++) {
+                GameLogic otherPlayer = this.createOther(x,y,min); //Ny spiller
+                otherPlayer.insertCoin(i,max); //Opdater spilleplade 
+                int currentMin = otherPlayer.minValue();
+                if (currentMin > v) {
+                    v = currentMin;
+                    bestAction = i;
+                }
+                
+            }
+
+return bestAction
+            
+        } else {
+        
+        }
     }
 
     
@@ -115,11 +140,30 @@ public class GameLogic implements IGameLogic {
     }
 
     public boolean terminalTest() {
-        return false;
+        counter -= 1;
+        if (counter <= 0) {
+            System.out.println("terminated.");
+            printBoard();
+            return true;
+        }
+        else return false;
+
     }
 
     public int utility() {
+
         return 1;
+    }
+
+    public void printBoard() {
+
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j<board[i].length; j++) {
+                System.out.print(" " + board[i][j]);
+            }
+            System.out.println();
+        }
+
     }
 
 }
