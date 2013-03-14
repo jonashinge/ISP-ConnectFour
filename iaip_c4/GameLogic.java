5f1a4d2f59b5efd1bd859b8cb3148db1fba16010
markgraydk <mail@markgray.dk>
2013-03-14 11:33:19 +0100

    new

--- iaip_c4/GameLogic.java
+++ iaip_c4/GameLogic.java
@@ -25,6 +25,7 @@ public class GameLogic implements IGameLogic {
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j<board[i].length; j++) {
                 this.board[i][j] = board[i][j];
+                
             }
         }
        // System.out.println("New object " + counter);
