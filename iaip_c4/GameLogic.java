ddffa54f3bf969589485fffea93a49c1cc8a20fb
johin <jonas.hinge@gmail.com>
2013-03-20 10:19:26 +0100

    vertical and horizontal working...

--- iaip_c4/GameLogic.java
+++ iaip_c4/GameLogic.java
@@ -35,7 +35,6 @@ public class GameLogic implements IGameLogic {
                 
             }
         }
-       // System.out.println("New object " + counter);
     }
     
 
@@ -49,40 +48,41 @@ public class GameLogic implements IGameLogic {
         this.playerID = playerID;
         //TODO Write your implementation for this method
         board = new int[x][y];
-
-        /*Node node = new Node(0,0,3);
-        horizontal.add(node);*/
     }
 	
     public Winner gameFinished() {
         //TODO Write your implementation for this method
-        System.out.println("Game gameFinished");
 
         Integer[] columns = getFreeColumns();
 
         // test print datastructure
-        for(Node n : horizontal) {
-            
-            System.out.print("x:" + n.x + " y:" + n.y + " playerid:" + n.playerID + ", ");
+        System.out.println("Horizontal:");
+        printDataStructure(horizontal);
+        System.out.println();
 
-            while(n.getNext() != null) {
-                System.out.print("x:" + n.getNext().x + " y:" + n.getNext().y + " playerid:" + n.getNext().playerID + ", ");
-                n = n.getNext();
-            }
-            System.out.println();
-        }
+        System.out.println("Vertical:");
+        printDataStructure(vertical);
+        System.out.println();
+
+        System.out.println("Right diagonal:");
+        printDataStructure(rightDiagonal);
+        System.out.println();
 
         if(columns != null && columns.length>0) {
             printBoard();
-            System.out.println("NOT NOT_FINISHED");
-            for (int i : columns) {
-                System.out.print(i +" ");
-            }
             System.out.println();
+            System.out.println("Game status:");
+            System.out.println("NOT FINISHED");
+            /*for (int i : columns) {
+                System.out.print(i +" ");
+            }*/
             return Winner.NOT_FINISHED;
         }
         else {
+            System.out.println();
             printBoard();
+            System.out.println();
+            System.out.println("Game status:");
             System.out.println("TIE");
             return Winner.TIE;
         }
@@ -94,19 +94,27 @@ public class GameLogic implements IGameLogic {
 
     public void insertVertical(int column, int row, int playerID) {
         Node n = new Node(column,row,playerID);
+        boolean inserted = false;
+
         if ( row == 0 ) {
             vertical.add(n);
         } else {
             for (Node node : vertical) {
                 Node tempNode = node;
-                while (tempNode.getNext() != null) {
+                while (true) {
                     if (tempNode.playerID == playerID && tempNode.x == column && tempNode.y == row-1) {
                         tempNode.setNext(n);     
+                        inserted = true;
                         break;               
                     }
                     tempNode = tempNode.getNext();
+                    if(tempNode==null) {
+                        break;
+                    }
                 }
             }
+            if(inserted==false)
+                vertical.add(n);
         }
     }
 
@@ -150,19 +158,28 @@ public class GameLogic implements IGameLogic {
 
         public void insertRightDiagonal(int column,int row, int playerID) {
         Node n = new Node(column,row,playerID);
+        boolean inserted = false;
 
         for (Node columnNode : rightDiagonal) {
-            if (columnNode.x == column - 1 ) {
+            if (columnNode.x == column - 1) {
                 Node tempNode = columnNode;
-                while (tempNode.getNext() != null) {
-                    if (tempNode.y == row-1 && tempNode.playerID == playerID) {
+                while (true) {
+                    if (tempNode.y == row-1 && tempNode.x == column-1 && tempNode.playerID == playerID) {
                         tempNode.setNext(n);                        
+                        inserted = true;
+                        break;
                     }
                     tempNode=tempNode.getNext();
+                    if(tempNode==null) {
+                        break;
+                    }
                 }
             }
 
         }
+
+        if(inserted==false)
+            rightDiagonal.add(n);
         
 
         Iterator<Node> i = rightDiagonal.iterator();
@@ -206,33 +223,6 @@ public class GameLogic implements IGameLogic {
         
     }
 
-    
-
-    /*      if (column != 0 || column != board.length -1) {
-            
-        }
-
-                while (i.hasNext()) {
-            Node iNode = i.next();
-
-            while (j.hasNext()) {
-                Node jNode = j.next();
-
-                if (iNode.y == jNode.y) {
-                    
-                    if (iNode.x == jNode.x-1 && iNode.playerID == jNode.playerID) {
-                        iNode.setNext(jNode);
-                        j.remove();
-                    } else {
-                        
-                    }
-
-                jNode= j.next();
-                }
-            }
-        }
-    */
-
     public void insertCoin(int column, int playerID) {
         //TODO Write your implementation for this method
         int free = getFreeRow(column);
@@ -275,7 +265,6 @@ public class GameLogic implements IGameLogic {
                     v = currentMin;
                     bestAction = columns[i];
                 } else if (currentMin == v && Math.random() >=0.5d) {
-                    System.out.println("random");
                     v = currentMin;
                     bestAction = columns[i];
 
@@ -297,7 +286,6 @@ public class GameLogic implements IGameLogic {
                     v = currentMax;
                     bestAction = columns[i];
                 } else if (currentMax == v && Math.random() >=0.5d) {
-                    System.out.println("random");
                     v = currentMax;
                     bestAction = columns[i];
 
@@ -309,6 +297,7 @@ public class GameLogic implements IGameLogic {
         if (bestAction <0)
             System.out.println("FEJL!!!");
         System.out.println("decideNextMove bestAction is " + bestAction);
+        System.out.println();
         return bestAction;
     }
 
@@ -398,13 +387,25 @@ public class GameLogic implements IGameLogic {
 
     public void printBoard() {
 
+        System.out.println("Board:");
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j<board[i].length; j++) {
                 System.out.print(" " + board[i][j]);
             }
             System.out.println();
         }
+    }
 
+    public void printDataStructure(ArrayList<Node> list) {
+
+        for(Node n : list) {
+            System.out.print("x:" + n.x + " y:" + n.y + " playerid:" + n.playerID + ", ");
+            while(n.getNext() != null) {
+                System.out.print("x:" + n.getNext().x + " y:" + n.getNext().y + " playerid:" + n.getNext().playerID + ", ");
+                n = n.getNext();
+            }
+            System.out.println();
+        }
     }
 
 }
